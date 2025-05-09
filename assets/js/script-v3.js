593c3aabab183678b4dfb9f4322b5fc342f1224c
Bahram <bahrampc@gmail.com>
2025-05-09 09:01:44 +0330

    script modified

--- assets/js/script-v3.js
+++ assets/js/script-v3.js
@@ -165,7 +165,13 @@ document.addEventListener('DOMContentLoaded', function() {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
+          'Accept': 'application/json',
+          'Access-Control-Allow-Origin': '*',
+          'Access-Control-Allow-Methods': 'POST',
+          'Access-Control-Allow-Headers': 'Content-Type'
         },
+        mode: 'cors',
+        credentials: 'include',
         body: JSON.stringify({
           action: 'message',
           name: name,
